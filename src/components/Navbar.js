7fb588d1ff68621a93f89b61b5f638027a08d46b
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-29 23:09:42 -0800

    added the css to the nav and logo and hamburger menu

---
+++ src/components/Navbar.js
@@ -0,0 +1,35 @@
+import React from "react";
+import logo from "../logo_eg.png";
+//React Font Awesome Import //
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faBars } from "@fortawesome/free-solid-svg-icons"
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand" href="#"><img className="logotop" src={logo} alt="logo..."/></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <FontAwesomeIcon icon={faBars} style={{ color: "#fff" }}/>
+            </button>
+
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul className="navbar-nav ml-auto">
+                    <li className="nav-item active">
+                        <a className="nav-link" href="#">About Me</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">My Skills</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">My Projects</a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link" href="#">Contact Me</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar
