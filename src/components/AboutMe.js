5b41237027a30824d97be62726f3ccc2e2199fc9
Ernesto Garcia <egarcia2020@yahoo.com>
2021-11-30 22:56:53 -0800

    latest code

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -1,11 +1,12 @@
 import React from 'react'
+import me from "../mememe.png"
 
 export const AboutMe = () => {
     return (
         <div className="header-wrapper-aboutme">
             <div className="main-info-aboutme">
                 <h2 className="aboutmename">About Me</h2>
-                <img src="./src/mememe.png"/>
+                <img id="imageme" src={me}></img>
                 <p className="intro">Full Stack Web Developer (MERN) with field experience in front end. Learning everyday and everyday I'm getting better with all languages I use. Fanatic of technology and I thrive to be not just a developer, but an outstanding and creative one.</p>
             </div>
         </div>
